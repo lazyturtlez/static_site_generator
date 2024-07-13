f3c8f229be2c2ef975770dd9d50bff6c1b0c708c
lazyturtlez <robertbracamonte803@gmail.com>
2024-07-12 18:06:13 -0700

    E: Added conversion logic from TextNode to LeafNode. Created additional unit tests to validate logic

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,7 +1,7 @@
 import unittest
 
 from textnode import TextNode
-
+from textnode import text_node_to_html_node
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -28,6 +28,22 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", "bold", "test url")
         self.assertEqual("TextNode(This is a text node, bold, test url)", repr(node))
 
+class TestTextNodeToHTMLNode(unittest.TestCase):
+
+    def test_text(self):
+        text_node = TextNode("test", "text")
+        html_node = text_node_to_html_node(text_node)
+        self.assertEqual(html_node.to_html(), "test")
+
+    def test_text_to_bold(self):
+        text_node = TextNode("test", "bold")
+        html_node = text_node_to_html_node(text_node)
+        self.assertEqual(html_node.to_html(), "<b>test</b>")
+
+    def test_image(self):
+        text_node = TextNode("test", "img", "www.test")
+        html_node = text_node_to_html_node(text_node)
+        self.assertEqual(html_node.props, {"src": "www.test", "alt": "test"})
 
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,3 +1,5 @@
+from htmlnode import LeafNode
+
 class TextNode():
     def __init__(self, text, text_type, url=None):
         self.text = text
@@ -13,6 +15,24 @@ class TextNode():
 
     def __repr__(self) -> str:
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
+
+def text_node_to_html_node(text_node: TextNode):
+        match text_node.text_type:
+            case "text":
+                return LeafNode(None, text_node.text)
+            case "bold":
+                return LeafNode("b", text_node.text)
+            case "italic":
+                return LeafNode("i", text_node.text)
+            case "code":
+                return LeafNode("code", text_node.text)
+            case "link":
+                return LeafNode("a", text_node.text, {"href": text_node.url})
+            case "img":
+                return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+            case _:
+                raise ValueError(f"Invalid text type: {text_node.text_type}")
+            
 
 
 def main():
